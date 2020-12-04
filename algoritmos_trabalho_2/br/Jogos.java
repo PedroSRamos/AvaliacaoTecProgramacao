269d88e3972559100e0c9dfcb0a510122ab64ec5
Pedro Henrique de Souza Ramos <36521649+PedroSRamos@users.noreply.github.com>
2020-12-04 14:35:59 -0300

    Add files via upload

---
+++ algoritmos_trabalho_2/br/Jogos.java
@@ -0,0 +1,9 @@
+package br.edu.univas.br;
+
+public class Jogos {
+	
+	public String mandante;
+	public String visitante;
+	public int gMandante;
+	public int gVisitante;		
+}
---
+++ algoritmos_trabalho_2/br/Times.java
@@ -0,0 +1,10 @@
+package br.edu.univas.br;
+
+public class Times {
+
+	public String nome;
+	public String estadoOrigem;
+	
+	public int pontos = 0;
+	public int saldoGols = 0;
+}
