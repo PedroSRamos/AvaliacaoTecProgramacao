4d953a5320be9f15e3f279cf17124f27e7d45a78
Pedro Henrique de Souza Ramos <36521649+PedroSRamos@users.noreply.github.com>
2023-12-09 19:05:20 -0300

    Delete algoritmos_trabalho_2 directory

--- algoritmos_trabalho_2/main/Points.java
+++
@@ -1,9 +0,0 @@
-package br.edu.univas.main;
-
-public class Points {
-
-	public int mastermindPoints = 0;
-	public int visitorPoints = 0;
-	public int amountGolsMast = 0;
-	public int amountGolsVisit = 0;
-}
--- algoritmos_trabalho_2/main/team.java
+++
@@ -1,9 +0,0 @@
-package br.edu.univas.main;
-
-public class Team {
-
-	public String name;
-	public String state;
-	public int points = 0;
-	public int amountGols = 0;
-}
